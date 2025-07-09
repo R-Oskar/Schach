fc7749f4c217ea73bc40d60e0319c8d8bde3a084
Maksim Tonda Backes <maksim.backes@avh.berlin>
2025-07-09 10:17:26 +0200

    commentar

--- src/Main/Play.java
+++ src/Main/Play.java
@@ -1,14 +1,13 @@
 package Main;
 
-import javax.swing.SwingUtilities;
-
 import UI.GUI;
+import javax.swing.SwingUtilities;
 
 public class Play {
     public static void main(String[] args) {
         startGame();
     }
-
+    // start the game here
     public static void startGame() {
         SwingUtilities.invokeLater(() -> {
             new GUI().setVisible(true);
