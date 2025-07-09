91449eebaf2bf56e80ed78e697c84a96cd401f73
Maksim Tonda Backes <maksim.backes@avh.berlin>
2025-07-09 10:47:42 +0200

    hard AI Framework

---
+++ src/AI/HardAI.java
@@ -0,0 +1,13 @@
+package AI;
+
+import Scripts.*;
+
+public class HardAI {
+    private static final int MAX_DEPTH = 3;
+
+    public Move getBestMove(Board board) {
+        Move bestMove = null;
+        
+        return bestMove;
+    }
+}
