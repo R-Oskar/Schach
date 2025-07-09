526f179d258fcd9a136835e07b84c845f2ce1f3e
Maksim Tonda Backes <maksim.backes@avh.berlin>
2025-07-09 13:33:34 +0200

    ken plan ob das Funktioniert

--- src/AI/AbstractAI.java
+++ src/AI/AbstractAI.java
@@ -2,13 +2,22 @@ package AI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
-import Pieces.Piece;
+import Pieces.*;
 import Pieces.PieceColor;
 import Scripts.Board;
 import Scripts.Move;
 
 abstract public class AbstractAI {
+    public static final HashMap<Piece, Integer> pieceValues = new HashMap<Piece, Integer>() {{
+        put(Piece.PAWN, 1);
+        put(Piece.KNIGHT, 3);
+        put(Piece.BISHOP, 3);
+        put(Piece.ROOK, 5);
+        put(Piece.QUEEN, 9);
+        put(Piece.KING, 0); // King is invaluable
+    }};
 
     public abstract Move getMove(Board board, PieceColor atTurn);
     
@@ -24,5 +33,15 @@ abstract public class AbstractAI {
             }
         }
         return allLegalMoves;
+    }
+    /**
+     * Evaluates the position of the board for the given player. Whit is positive, black is negative.
+     */
+    public static int positionEvaluation(Board board, PieceColor atTurn) {
+        int evaluation = 0;
+        board.getPieces()
+
+        return evaluation;
+
     }
 }
